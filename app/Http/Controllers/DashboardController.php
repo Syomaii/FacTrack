418216ff9253636cada48948d65a8051c9a6fd15
Syomai <chrischanjay29@gmail.com>
2024-09-08 21:55:57 +0800

    edited dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function countForWidgets(){
+        $count = User::count();
+
+        $data = [
+            'count' => $count,
+            'title' => 'Dashboard'
+        ];
+
+        return $data; 
+
+    }
 }
