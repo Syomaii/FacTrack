da975484681c621d2064d11e23e3611adb2d80e9
Syomai <chrischanjay29@gmail.com>
2024-09-17 13:29:29 +0800

    added comments

--- database/migrations/2024_07_31_181529_create_users_table.php
+++ database/migrations/2024_07_31_181529_create_users_table.php
@@ -4,10 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * Create users table migration.
+ *
+ * This migration creates the `users` table with the following columns:
+ * - `id`: auto-incrementing primary key
+ * - `designation_id`: unsigned integer referencing the `id` column on the `designations` table
+ * - `office_id`: unsigned integer referencing the `id` column on the `offices` table
+ * - `firstname`: string with a maximum length of 50 characters
+ * - `lastname`: string with a maximum length of 50 characters
+ * - `email`: unique string with a maximum length of 50 characters
+ * - `password`: string with a maximum length of 255 characters
+ * - `mobile_no`: string with a maximum length of 50 characters
+ * - `image`: nullable string with a maximum length of 255 characters
+ * - `status`: string with a maximum length of 50 characters
+ * - `type`: string with a maximum length of 50 characters
+ * - `created_at` and `updated_at`: timestamp columns for tracking creation and update times
+ *
+ * Example:
+ * ```bash
+ * php artisan migrate
+ * ```
+ * This will create the `users` table with the specified columns.
+ */
 return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * Creates the `users` table with the specified columns.
      */
     public function up(): void
     {
@@ -15,6 +40,7 @@ return new class extends Migration
             $table->increments('id');
             $table->unsignedInteger('designation_id');
             $table->unsignedInteger('office_id');
+            $table->index('office_id');
             $table->foreign('designation_id')->references('id')->on('designations')->onDelete('cascade');
             $table->foreign('office_id')->references('id')->on('offices')->onDelete('cascade');
             $table->string('firstname', 50);
@@ -31,6 +57,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * Drops the `users` table.
      */
     public function down(): void
     {
