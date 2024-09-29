de08d17d2e3bfaa96df6801b0aead0b8e73e5a5e
Syomaii <chrischanjay29@gmail.com>
2024-09-29 23:59:47 +0800

    updated the timeline model

--- app/Models/Timeline.php
+++ app/Models/Timeline.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Timeline extends Model
 {
     use HasFactory;
+
+    protected $table = "timeline";
+
+    protected $fillable = [
+        'equipment_id',
+        'user_id',
+        'status',
+        'remarks',
+    ];
+
+    public function equipment() {
+        return $this->belongsTo(Equipment::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
