a12d47afe60693c46b5972b1a45c3bb59c98813b
Syomaii <chrischanjay29@gmail.com>
2024-09-29 23:58:44 +0800

    updated the report controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    
+    public function borrowedEquipmentReports(){
+        return view('reports/borrowed_equipments')->with('title', 'Borrowed Equipments');
+    }
+
+    public function maintenancedEquipmentReports(){
+        return view('reports/maintenanced_equipments.blade.php')->with('title', 'Maintained Equipments');
+    }
+
+    public function repairedEquipmentReports(){
+        return view('reports/repaired_equipments')->with('title', 'Repaired Equipments');
+    }
 }
