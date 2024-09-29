62e162a268abbf2348c0da3bd2d99a61d0b8a57c
Syomaii <chrischanjay29@gmail.com>
2024-09-30 00:06:36 +0800

    updated the view for the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,16 @@
   <main class="dashboard-main">
     <x-navbar />
 
+    @if (session('loginUserSuccessfully'))
+        <div
+            class="alert alert-success bg-success-100 text-success-600 border-success-600 border-start-width-4-px border-top-0 border-end-0 border-bottom-0 px-24 py-13 mb-3 fw-semibold text-lg radius-4 d-flex align-items-center justify-content-between">
+            <div class="d-flex align-items-center gap-2">
+                <iconify-icon icon="akar-icons:double-check" class="icon text-xl"></iconify-icon>
+                {{ session('loginUserSuccessfully') }}
+            </div>
+        </div>
+    @endif
+
     <div class="dashboard-main-body">
       <div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
         <h6 class="fw-semibold mb-0">Dashboard</h6>
