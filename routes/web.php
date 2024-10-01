1d55bc5baf32a2152e187c5fa4799897792f6492
Syomaii <chrischanjay29@gmail.com>
2024-10-01 10:14:08 +0800

    modified the routes for borrowing

--- routes/web.php
+++ routes/web.php
@@ -66,9 +66,9 @@ Route::middleware(['auth'])->group(function () {
         // Route::get('/borrow-details/{code}', [BorrowerController::class, 'showDetails'])->name('borrow_details');
 
         Route::get('/borrow-equipment', [BorrowerController::class, 'borrowEquipment'])->name('borrow_equipment');
-        Route::post('/borrow-equipment', [BorrowerController::class, 'borrowerFormPost']);
-        Route::get('/borrow-details/{code}', [BorrowerController::class, 'showBorrowDetails'])->name('borrow.details');
-        Route::post('/submit-borrow', [BorrowerController::class, 'submitBorrow'])->name('submit.borrow');
+        // Route::post('/borrow-equipment', [BorrowerController::class, 'borrowerFormPost']);
+        Route::get('/borrow-details/{code}', [BorrowerController::class, 'showBorrowDetails'])->name('borrow_details');
+        Route::post('/borrow-details/{code}', [BorrowerController::class, 'submitBorrow'])->name('borrow-equipment-post');
     });
 
 
