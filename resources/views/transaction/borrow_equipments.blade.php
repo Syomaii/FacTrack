6b9af863073e92e391c325e8154f075f05d953b2
Syomaii <chrischanjay29@gmail.com>
2024-10-24 13:03:05 +0800

    updated the borrow_equipments page

--- resources/views/transaction/borrow_equipments.blade.php
+++ resources/views/transaction/borrow_equipments.blade.php
@@ -9,7 +9,7 @@
             <h6 class="fw-semibold mb-0">Borrow Equipment</h6>
             <ul class="d-flex align-items-center gap-2">
                 <li class="fw-medium">
-                    <a href="index.html" class="d-flex align-items-center gap-1 hover-text-primary">
+                    <a href="{{ route('dashboard') }}" class="d-flex align-items-center gap-1 hover-text-primary">
                         <iconify-icon icon="solar:home-smile-angle-outline" class="icon text-lg"></iconify-icon>
                         Dashboard
                     </a>
