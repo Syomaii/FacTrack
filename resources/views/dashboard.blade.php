da2c7c15d1d83b1ba65ef3fbc5698a5390335c5e
Syomaii <chrischanjay29@gmail.com>
2024-11-04 18:12:56 +0800

    updated the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,15 +4,24 @@
 <main class="dashboard-main">
     <x-navbar />
 
-    @if (session('loginUserSuccessfully'))
-        <div
-            class="alert alert-success bg-success-100 text-success-600 border-success-600 border-start-width-4-px border-top-0 border-end-0 border-bottom-0 px-24 py-13 mb-3 fw-semibold text-lg radius-4 d-flex align-items-center justify-content-between">
+    @if (session('newUser'))
+        <div class="alert alert-warning bg-warning-100 text-warning-600 border-warning-600 border-start-width-4-px border-top-0 border-end-0 border-bottom-0 px-24 py-13 mb-3 fw-semibold text-lg radius-4 d-flex align-items-center justify-content-between">
+            <div class="d-flex align-items-center gap-2">
+                <iconify-icon icon="akar-icons:double-check" class="icon text-xl"></iconify-icon>
+                {{ session('newUser') }}
+            </div>
+        </div>
+    @elseif (session('loginUserSuccessfully'))
+        <div class="alert alert-success bg-success-100 text-success-600 border-success-600 border-start-width-4-px border-top-0 border-end-0 border-bottom-0 px-24 py-13 mb-3 fw-semibold text-lg radius-4 d-flex align-items-center justify-content-between">
             <div class="d-flex align-items-center gap-2">
                 <iconify-icon icon="akar-icons:double-check" class="icon text-xl"></iconify-icon>
                 {{ session('loginUserSuccessfully') }}
             </div>
         </div>
     @endif
+
+
+    
 
     <div class="dashboard-main-body">
         <div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
