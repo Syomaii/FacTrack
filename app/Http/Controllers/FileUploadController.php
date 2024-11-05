14c02f17443c1fb98b0e178cdcd4125c782cc2ec
Syomaii <chrischanjay29@gmail.com>
2024-11-05 23:42:32 +0800

    updated the FileUploadController

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -9,12 +9,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log as FacadesLog;
 use Laravel\Prompts\Concerns\Fallback;
 use Maatwebsite\Excel\Facades\Excel;
-use Maatwebsite\Excel\Jobs\ProxyFailures;
 
 class FileUploadController extends Controller
 {
     public function importStudents(Request $request)
     {
+        $request->validate(['file' => 'required|file|mimes:xlsx,xls|max:2048', ]);
         $file = $request->file("file")->store('import');
 
         $import = new StudentsImport();
@@ -23,9 +23,19 @@ class FileUploadController extends Controller
         // Check if there are any failures
         if ($import->failures()->isNotEmpty()) {
             FacadesLog::info('Import Failures: ', $import->failures()->toArray());
-            return back()->with('failures', $import->failures());
+
+            $failure = $import->failures();
+            // dd($failure);
+            session()->flash('abc', $failure);
+            session()->keep(['abc']);
+
+            // dd(session('failure'));
+            // return redirect()->route('import.file');
+            return view('imports.students', ['abc' => $failure, 'title' => 'Import']);
+
         }
-        return back()->withStatus('Successfully uploaded and imported the file.');
+        
+        return redirect()->back()->with('success','Successfully uploaded and imported the file.');
     }
 
 
