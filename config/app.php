f7c4b00d8bb6021266346da3802bf1b7b53ed5e1
Syomaii <chrischanjay29@gmail.com>
2024-11-07 22:05:51 +0800

    updated the app.php file for the timezone

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'Asia/Manila'),
+    'timezone' => 'Asia/Manila',
 
     /*
     |--------------------------------------------------------------------------
